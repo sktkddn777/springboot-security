079d8fa3dd117da9f12c1750bebe4330932c1dca
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-10-24 16:49:40 +0900

    Add: join
    join form 을 통해 회원가입 할 수 있게

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,20 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({"", "/"})
     public String index() {
@@ -28,13 +37,23 @@ public class IndexController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String loginForm() {
         return "login";
     }
 
     @GetMapping("/join")
-    public String join() {
+    public String joinForm() {
         return "join";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        user.setRole("ROLE_USER");
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        userRepository.save(user); // 비밀번호 그대로 노출됨, 패스워드 암호화 필요
+
+        return "redirect:/login";
     }
 
     @GetMapping("/joinProc")
